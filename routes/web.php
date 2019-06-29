0a472bf686d398221731ff57f3cae422ab478589
Minhaj <minhajulmubarok@gmail.com>
2019-06-29 09:58:38 +0700

    blank laravel

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tes',function(){
+    return view('admin.dashboard');
+}); 
+Route::get('/masuk',function(){
+    return view('admin.login');
+}); 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
